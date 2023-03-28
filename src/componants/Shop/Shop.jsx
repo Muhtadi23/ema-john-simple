78db2f190955fae6716823069db6b1d0ae9f9bc4
Muhtadi23 <miranmuhtadi121@gmail.com>
2023-03-28 15:24:39 +0600

    module 51 done

--- src/componants/Shop/Shop.jsx
+++ src/componants/Shop/Shop.jsx
@@ -35,8 +35,22 @@ const Shop = () => {
     }, [products])
 
     const handleAddToCart = (product) => {
+        // const cart.push(product)
+        let newCart = [];
+        // const newCart = [...cart, product];
+        // if product doesnt exist in the cart, then set quantity = 1;
+        // if exist update quantity by 1;
+        const exist = cart.find(pd => pd.id === product.id);
+        if(!exist){
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }
+        else{
+            exist.quantity = exist.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining, exist];
+        }
 
-        const newCart = [...cart, product];
         setCart(newCart);
         addToDb(product.id)
     }
